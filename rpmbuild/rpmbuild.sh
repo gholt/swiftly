801cc59b393874224d0a05b595c1098debd21c6f
gholt <z-launchpad@brim.net>
2013-03-08 01:17:43 +0000

    Added rpmbuild subdir; I am not that familar with rpms, so if any have better ideas how to do this, let me know. :)

---
+++ rpmbuild/rpmbuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+topdir=`dirname $0`
+topdir=`realpath $topdir`
+cd ${topdir}/..
+VERSION=`grep "VERSION = '" swiftly/__init__.py | cut -d"'" -f2`
+mkdir -p rpmbuild/SOURCES
+git archive --format tar.gz --prefix swiftly-${VERSION}/ -o rpmbuild/SOURCES/swiftly-${VERSION}.tar.gz HEAD
+cd rpmbuild
+rpmbuild --define "_topdir $topdir" -v -ba --clean SPECS/swiftly.spec
